cddea8d9d4060c0f9b3241eb3269d9390de83672
SunLionAhh <2468872989@qq.com>
2020-09-08 11:27:45 +0800

    init

---
+++ src/main/java/com/project/shopboot/ShopBootApplication.java
@@ -0,0 +1,13 @@
+package com.project.shopboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/shopboot/ShopBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.shopboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
